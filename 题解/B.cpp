262c846e411e580660897ded238ef9ece251f8b5
youthv587 <43020597+youthv587@users.noreply.github.com>
2019-05-13 18:02:14 +0800

    Update B.cpp

--- 题解/B.cpp
+++ 题解/B.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+//#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+int n,a,t,count;
+
+
+while(cin>>n){
+count=0;          
+ for(int i=1;i<=n;i++){
+         a=i;
+  while(a>8){
+  t=a%10;if(t==9)count++;
+  a=a/10;
+            }
+            
+}
+cout<<count<<endl;
+}
+
+return 0;
+}
 
