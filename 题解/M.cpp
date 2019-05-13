27e2c59120de83cd3d48bb8b20d3f5de48a63152
youthv587 <43020597+youthv587@users.noreply.github.com>
2019-05-13 18:36:18 +0800

    Create M.cpp

---
+++ 题解/M.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+string str;
+
+
+while(cin>>str){
+                if(str=="YD")cout<<34<<endl;
+                else if(str=="CJ")cout<<54<<endl;
+                else if(str=="QMJ")cout<<24<<endl;
+                else if(str=="LDJ")cout<<34<<endl;
+                else if(str=="DWJ")cout<<96<<endl;
+                else if(str=="ZQJ")cout<<16<<endl;
+                else cout<<"Happy  New  Year !"<<endl;
+                }
+return 0;
+}
