7ba820bdfe708ead2a669ce7dcbe227e8f9f8383
youthv587 <43020597+youthv587@users.noreply.github.com>
2019-05-13 18:48:25 +0800

    Update A.cpp

--- 题解/A.cpp
+++ 题解/A.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str;
+    while(cin>>str){
+	int s=str.size();
+	for(int i=0;i<s;i++){
+	
+	    for(int j=i;j<i+s;j++){
+	    	cout<<str[j%s];
+	    }
+	    cout<<endl;
+	}
+	
+    }
+    
+    
+    return 0;
+}
